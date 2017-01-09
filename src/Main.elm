a3b8399c5d5c098b2b710587d9b1d8dd57fcc1f7
Max Burri <burri@puzzle.ch>
2017-01-09 16:31:31 +0100

    moved view to separate module

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,10 @@
 module Main exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (value, class, type_)
-import Html.Events exposing (onClick, onInput)
-import Contact exposing (Contact)
+import Html exposing (Html)
 import Model exposing (Model, init)
 import Messages exposing (..)
 import Update exposing (..)
+import View exposing (view)
 
 
 main =
@@ -19,97 +17,9 @@ main =
 
 
 
--- UPDATE
 -- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-    div [ class "container" ]
-        [ div [ class "row " ] [ h1 [] [ text "Contacts" ] ]
-        , viewContactPanel model.selectedContact
-        , viewToolbar
-        , (viewContacts model.contacts)
-        ]
-
-
-viewToolbar : Html Msg
-viewToolbar =
-    div [ class "row" ]
-        [ button
-            [ class "button button-outline"
-            , onClick AddContact
-            ]
-            [ text "Add" ]
-        ]
-
-
-viewContactPanel : Maybe Contact -> Html Msg
-viewContactPanel contact =
-    case contact of
-        Nothing ->
-            text ""
-
-        Just c ->
-            div [ class "contact-panel" ]
-                [ viewInput c
-                , button [ onClick SaveContact ] [ text "Save" ]
-                , button [ onClick Cancel ] [ text "Cancel" ]
-                , button [ onClick DeleteContact ] [ text "Delete" ]
-                ]
-
-
-viewInput : Contact -> Html Msg
-viewInput contact =
-    fieldset []
-        [ label [] [ text "First name" ]
-        , input
-            [ type_ "text"
-            , onInput (Change Firstname)
-            , value contact.firstname
-            ]
-            []
-        , label [] [ text "Last name" ]
-        , input
-            [ type_ "text"
-            , value contact.lastname
-            , onInput (Change Lastname)
-            ]
-            []
-        , label [] [ text "Phone" ]
-        , input
-            [ type_ "text"
-            , onInput (Change Phone)
-            , value contact.phone
-            ]
-            []
-        ]
-
-
-viewContacts : List Contact -> Html Msg
-viewContacts contacts =
-    table []
-        [ tbody []
-            (List.map viewContact contacts)
-        ]
-
-
-viewContact : Contact -> Html Msg
-viewContact contact =
-    tr [ onClick <| Select contact ]
-        [ td []
-            [ text contact.firstname ]
-        , td []
-            [ text contact.lastname ]
-        , td []
-            [ text contact.phone ]
-        ]
---
+++ src/View.elm
@@ -0,0 +1,92 @@
+module View exposing (view)
+
+import Html exposing (..)
+import Html.Attributes exposing (value, class, type_)
+import Html.Events exposing (onClick, onInput)
+import Contact exposing (Contact)
+import Contact exposing (Contact)
+import Model exposing (Model)
+import Messages exposing (..)
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "container" ]
+        [ div [ class "row " ] [ h1 [] [ text "Contacts" ] ]
+        , viewContactPanel model.selectedContact
+        , viewToolbar
+        , (viewContacts model.contacts)
+        ]
+
+
+viewToolbar : Html Msg
+viewToolbar =
+    div [ class "row" ]
+        [ button
+            [ class "button button-outline"
+            , onClick AddContact
+            ]
+            [ text "Add" ]
+        ]
+
+
+viewContactPanel : Maybe Contact -> Html Msg
+viewContactPanel contact =
+    case contact of
+        Nothing ->
+            text ""
+
+        Just c ->
+            div [ class "contact-panel" ]
+                [ viewInput c
+                , button [ onClick SaveContact ] [ text "Save" ]
+                , button [ onClick Cancel ] [ text "Cancel" ]
+                , button [ onClick DeleteContact ] [ text "Delete" ]
+                ]
+
+
+viewInput : Contact -> Html Msg
+viewInput contact =
+    fieldset []
+        [ label [] [ text "First name" ]
+        , input
+            [ type_ "text"
+            , onInput (Change Firstname)
+            , value contact.firstname
+            ]
+            []
+        , label [] [ text "Last name" ]
+        , input
+            [ type_ "text"
+            , value contact.lastname
+            , onInput (Change Lastname)
+            ]
+            []
+        , label [] [ text "Phone" ]
+        , input
+            [ type_ "text"
+            , onInput (Change Phone)
+            , value contact.phone
+            ]
+            []
+        ]
+
+
+viewContacts : List Contact -> Html Msg
+viewContacts contacts =
+    table []
+        [ tbody []
+            (List.map viewContact contacts)
+        ]
+
+
+viewContact : Contact -> Html Msg
+viewContact contact =
+    tr [ onClick <| Select contact ]
+        [ td []
+            [ text contact.firstname ]
+        , td []
+            [ text contact.lastname ]
+        , td []
+            [ text contact.phone ]
+        ]
